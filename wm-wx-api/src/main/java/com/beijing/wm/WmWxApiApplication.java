a6b8d0448d1823e55f2eb553f70c441f02966f00
yang <yangguoqing0616@aliyun.com>
2020-12-07 01:26:02 +0800

    wx-api

---
+++ wm-wx-api/src/main/java/com/beijing/wm/WmWxApiApplication.java
@@ -0,0 +1,13 @@
+package com.beijing.wm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmWxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmWxApiApplication.class, args);
+    }
+
+}
---
+++ wm-wx-api/src/test/java/com/beijing/wm/WmWxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beijing.wm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmWxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
